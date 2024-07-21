2b8691decda725c22057426dcdb85a7a051d0da9
Carlos Hernandez <carlosandresh925@gmail.com>
2024-07-20 18:05:40 -0700

    Added new jokes pages

---
+++ app/routes/jokes.$jokeId.tsx
@@ -0,0 +1,10 @@
+export default function JokeIdRoute() {
+    return (
+        <div className="">
+            <p>Here's your hilarious joke:</p>
+            <p>
+                Sample Text
+            </p>
+        </div>
+    );
+}
